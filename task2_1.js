d2f8f0b08668db893e8a7be20c9d5eac30e08701
Gobedan <jobedano@gmail.com>
2020-10-10 13:13:10 +0400

    8 out of 8 tasks done

--- task2_1.js
+++ task2_1.js
@@ -1,7 +1,7 @@
 function createTextObjects(string) {
     result = [];
     string.trim().split(' ').forEach(function(word){
-        newObject = {
+        const newObject = {
             word: word,
             length: word.length,
             isCapitalized: !!word[0].match(/[A-Z]/)
---
+++ task2_7.js
@@ -0,0 +1,6 @@
+const newObject = {};
+newObject.name = 'Alex';
+newObject.lastName = 'Smith';
+newObject.name = 'Bob';
+delete newObject.name;
+delete newObject.lastName;
