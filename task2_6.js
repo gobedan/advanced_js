54da28f50089e38035d484db598d35242c99e8a3
Valentin Markin <jobedano@gmail.com>
2020-10-12 18:13:17 +0400

    Revert "random sort fixed"
    This reverts commit e6f355122eb4256c6790a3fed4e66a1c18c1bdb4.

--- task2_6.js
+++ task2_6.js
@@ -1,5 +1,8 @@
 function sortRandom(array){
-    return array.sort(function(a ,b) {
-        return 0.5 - Math.random();
+    if (Math.random() > 0.5) return array.sort(function(a ,b) {
+        return a - b;
+    });
+    else return array.sort(function(a, b) {
+        return b - a; 
     });
 }
