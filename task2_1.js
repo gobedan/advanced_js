60deb0fba9f6b5b48ee24d6ee21ecb79975cee9c
Valentin Markin <jobedano@gmail.com>
2020-10-12 18:13:17 +0400

    Revert "8 out of 8 tasks done"
    This reverts commit 23a43abf5279f1f0bed227dbe7e4ee9c73c23c75.

--- task2_1.js
+++ task2_1.js
@@ -1,7 +1,7 @@
 function createTextObjects(string) {
     result = [];
     string.trim().split(' ').forEach(function(word){
-        const newObject = {
+        newObject = {
             word: word,
             length: word.length,
             isCapitalized: !!word[0].match(/[A-Z]/)
--- task2_7.js
+++
@@ -1,6 +0,0 @@
-const newObject = {};
-newObject.name = 'Alex';
-newObject.lastName = 'Smith';
-newObject.name = 'Bob';
-delete newObject.name;
-delete newObject.lastName;
