6546c0996ad85aa691f8179169367629efc9d5d7
Gobedan <jobedano@gmail.com>
2020-10-15 10:32:13 +0400

    refactored task3_1 with closures

--- task3_1.js
+++ task3_1.js
@@ -1,14 +1,8 @@
-function MoneyBox() {
-    Object.defineProperty(this, '_coins', {
-        value: 0,
-        configurable: true
-    });
-    // тут я ничего умнее не придумал, чтобы запретить доступ из вне, выглядит как анти-паттерн
-    this.addCoin = () => {
-        Object.defineProperty(this, '_coins', { writable: true });
-        this._coins++;
-        Object.defineProperty(this, '_coins', { writable: false });
+ function MoneyBox() {
+    let coins = 0; 
+    this.addCoin = () => { 
+        coins++;
         return null;
     };
-    this.getAmount = () => this._coins;
-}
+    this.getAmount = () => coins; 
+ }
