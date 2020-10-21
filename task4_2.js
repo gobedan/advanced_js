b680c917c92305ab3dfda98c1dcd403613c79245
Gobedan <jobedano@gmail.com>
2020-10-21 18:25:14 +0400

    rectangle constructor insted of plain object

--- task4_2.js
+++ task4_2.js
@@ -1,25 +1,21 @@
-const rectangle = {
-    a_side: 10,
-    b_side: 20,
-    perimeter: function() {
-        return 2 * (this.a_side + this.b_side); 
-    },
-    square: function() {
-        return this.a_side * this.b_side;
-    }
-}
-
-const square = Object.create(rectangle, {
-    a_side: {
-        value: 10,
-        writable: true
-    },
-    b_side: {
-        get: function() {
-            return this.a_side;
+function Rectangle(a, b) {
+    this.a_side = a;
+    this.b_side = b;
+    Object.defineProperty(this, 'perimeter', {
+        get: function(){
+            return 2 * (this.a_side + this.b_side);
         }
-    }
-});
+    });
+    Object.defineProperty(this, 'square', {
+        get: function(){
+            return this.a_side * this.b_side;
+        }
+    });
+};
+
+function Square(a) {
+    this.__proto__ = new Rectangle(a, a);
+};
 
 
 
