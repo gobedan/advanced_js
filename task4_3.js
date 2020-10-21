ed5f07e496fd93fec21186f394baa63175e4dc0f
Gobedan <jobedano@gmail.com>
2020-10-21 18:30:29 +0400

    employee constructor instead of plain object implemented

--- task4_3.js
+++ task4_3.js
@@ -1,52 +1,31 @@
-/* const john = {
-    name: "John",
-    lastName: "Smith",
-    position: "Senior engineer",
-    startDate: "10.10.1990",
-    endDate: "10.10.2000",
-    baseSalary: "10000",
-    salaryCurrency: "$",
-    location: "Russia",
-    department: "IT",
-    phoneNumber: "+1234567890",
-    eat: function() {},
-    sleep: function() {},
-    callFriend: function() {},
-    writeReport: function() {},
-    organizeMeeting: function () {},
-    retire: function () {},
-    startVacation: function () {}
-  };
- */
+function Human() {
+   this.name = "John";
+   this.lastName = "Smith";
+   this.location = "Russia";
+   this.phoneNumber = "+1234567890";
+   this.eat = function() {};
+   this.sleep = function() {};
+   this.callFriend = function() {};
+};
 
-const human = {
-   name: "John",
-   lastName: "Smith",
-   location: "Russia",
-   phoneNumber: "+1234567890",
-   eat: function() {},
-   sleep: function() {},
-   callFriend: function() {}
-}
+function Employee() {
+   this.__proto__ = new Human();
+   this.position = "Senior engineer";
+   this.startDate = "10.10.1990";
+   this.department = "IT";
+};
 
-const employee = {
-   __proto__: human,
-   position: "Senior engineer",
-   startDate: "10.10.1990",
-   department: "IT"
-}
+function ActiveEmployee() {
+   this.__proto__ = new Employee();
+   this.baseSalary = "10000";
+   this.salaryCurrency = "$";
+   this.writeReport = function() {};
+   this.organizeMeeting = function () {};
+   this.retire = function () {};
+   this.startVacation = function () {};
+};
 
-const active_employee = {
-   __proto__: employee,
-   baseSalary: "10000",
-   salaryCurrency: "$",
-   writeReport: function() {},
-   organizeMeeting: function () {},
-   retire: function () {},
-   startVacation: function () {}
-}
-
-const former_employee = {
-    __proto__: employee,
-    endDate: "10.10.2000"
-}
+function FormerEmployee() {
+   this.__proto__ = new Employee();
+   this.endDate = "10.10.2000";
+};
